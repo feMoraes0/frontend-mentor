7d2381339ad91170d6d1a9c4034908f8ad2c645c
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-12-22 21:09:46 -0300

    fix: moves test folder and create main folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../src/App';
+import App from '../main/App';
 
-describe('App component', () => {
+describe('App', () => {
   it('Should have page title with Projects string', () => {
     render(<App />);
     const pageTitle = screen.getByText(/projects/i);
