a37342871f8e6f332fda5d62dfbc87fb180ae121
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-12-25 19:32:36 -0300

    refactor(app): moves app files to App folder and starts to use styled-component

---
+++ src/main/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Header, HeaderText }  from './style';
+
+function App() {
+  return (
+    <Container>
+      <Header>
+        <HeaderText>Projects</HeaderText>
+      </Header>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/main/App/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.main``;
+
+export const Header = styled.header`
+  margin: 2rem auto;
+  text-align: center;
+  width: 100%;
+`;
+
+export const HeaderText = styled.h1`
+  font-size: 3rem;
+  font-weight: 400;
+  line-height: 3rem;
+`;
