d4a618aa411b45979786c0dd4a5d77bfcecca43a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-12-26 18:48:37 -0300

    test(app): adds app path test and moves render to internal test scope

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../main/App';
+import App, { AppPath } from '../main/App';
 
 describe('App', () => {
-  render(<App />);
+  it('Should app path be equal /', () => {
+    // THEN
+    expect(AppPath).toBe('/');
+  });
 
   it('Should have page title with Projects string', () => {
+    // SETUP
+    render(<App />);
     // GIVEN
     const pageTitle = screen.getByText(/projects/i);
     // THEN
